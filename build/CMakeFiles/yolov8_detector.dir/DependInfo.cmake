d37f902e7320adbb18c1afedb032e6196f8e6015
Tomás Morais Soares <tomas.soares@tum.de>
2025-06-01 00:28:44 +0200

    missing files from last commit

---
+++ build/CMakeFiles/yolov8_detector.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tomas/Projects/realtime-military-vehicle-detection/src/cpp/image_utils.cpp" "CMakeFiles/yolov8_detector.dir/src/cpp/image_utils.cpp.o" "gcc" "CMakeFiles/yolov8_detector.dir/src/cpp/image_utils.cpp.o.d"
+  "/home/tomas/Projects/realtime-military-vehicle-detection/src/cpp/main.cpp" "CMakeFiles/yolov8_detector.dir/src/cpp/main.cpp.o" "gcc" "CMakeFiles/yolov8_detector.dir/src/cpp/main.cpp.o.d"
+  "" "yolov8_detector" "gcc" "CMakeFiles/yolov8_detector.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/yolov8_detector.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov8_detector.dir/link.d"
+  "CMakeFiles/yolov8_detector.dir/src/cpp/image_utils.cpp.o"
+  "CMakeFiles/yolov8_detector.dir/src/cpp/image_utils.cpp.o.d"
+  "CMakeFiles/yolov8_detector.dir/src/cpp/main.cpp.o"
+  "CMakeFiles/yolov8_detector.dir/src/cpp/main.cpp.o.d"
+  "yolov8_detector"
+  "yolov8_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov8_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
