c69ee2bbd51a93a134e51926c42b186b01827c46
Martin Jardim Steinmayer <martin.j.steinmayer@gmail.com>
2025-05-31 16:19:27 +0200

    added yolo export

--- guardian_sight.py
+++ guardian_sight.py
@@ -1,1 +1,13 @@
 from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+
+# Export the model
+model.export(format='onnx', 
+            batch=1, 
+            device='cpu', 
+            simplify=True, 
+            imgsz=320, 
+            dynamic=False,
+            nms=True,
+            optimize=True)
